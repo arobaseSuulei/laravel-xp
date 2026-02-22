3c44d56a14c552ad13b4a58e9d707e434584d1fb
suleyjs <souleymanediallo2004@outlook.fr>
2026-02-22 03:56:15 +0100

    database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <h2> Your ideas </h2>
         @foreach($ideas as $idea)
 
-            <li> {{$idea->name}} </li>
+            <li> {{$idea->rating}} </li>
 
         @endforeach
     </div>
